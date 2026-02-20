7543bf7fb7481d57ad5b9978d7fa9103fdf71b63
hebcodee <herberth.ferreira6@gmail.com>
2026-02-20 17:43:48 -0300

    Initial commit

---
+++ src/main/java/com/myorg/AwsInfraApp.java
@@ -0,0 +1,14 @@
+package com.myorg;
+
+import software.amazon.awscdk.App;
+
+public class AwsInfraApp {
+    public static void main(final String[] args) {
+        App app = new App();
+
+
+
+        app.synth();
+    }
+}
+
