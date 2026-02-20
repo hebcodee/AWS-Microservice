f328e56810dd74e8d668a5b6cb71e13db9e89280
hebcodee <herberth.ferreira6@gmail.com>
2026-02-20 18:33:18 -0300

    feat: Add Auto Scaling

--- src/main/java/com/myorg/AwsServicesStack.java
+++ src/main/java/com/myorg/AwsServicesStack.java
@@ -1,13 +1,8 @@
 package com.myorg;
 
-import software.amazon.awscdk.Fn;
-import software.amazon.awscdk.RemovalPolicy;
-import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
-import software.amazon.awscdk.services.ecs.Cluster;
-import software.amazon.awscdk.services.ecs.ContainerImage;
-import software.amazon.awscdk.services.ecs.LogDriver;
+import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.applicationautoscaling.EnableScalingProps;
+import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
 import software.amazon.awscdk.services.logs.LogGroup;
@@ -31,7 +26,7 @@ public class AwsServicesStack extends Stack {
         authentication.put("SPRING_DATASOURCE_PASSWORD", Fn.importValue("pedidos-db-senha"));
 
 
-        ApplicationLoadBalancedFargateService.Builder.create(this, "AwsService")
+        ApplicationLoadBalancedFargateService awsService = ApplicationLoadBalancedFargateService.Builder.create(this, "AwsService")
                 .serviceName("aws-service")
                 .cluster(cluster)           // Required
                 .cpu(256)                   // Default is 256
@@ -55,5 +50,22 @@ public class AwsServicesStack extends Stack {
                 .memoryLimitMiB(512)       // Default is 512
                 .publicLoadBalancer(true)   // Default is true
                 .build();
+
+        ScalableTaskCount scalableTarget = awsService.getService().autoScaleTaskCount(EnableScalingProps.builder()
+                .minCapacity(1)
+                .maxCapacity(3)
+                .build());
+
+        scalableTarget.scaleOnCpuUtilization("CpuScaling", CpuUtilizationScalingProps.builder()
+                .targetUtilizationPercent(70)
+                .scaleInCooldown(Duration.minutes(3))
+                .scaleOutCooldown(Duration.minutes(2))
+                .build());
+        scalableTarget.scaleOnMemoryUtilization("MemoryScaling", MemoryUtilizationScalingProps.builder()
+                .targetUtilizationPercent(65)
+                .scaleInCooldown(Duration.minutes(3))
+                .scaleOutCooldown(Duration.minutes(2))
+                .build());
+
     }
 }
